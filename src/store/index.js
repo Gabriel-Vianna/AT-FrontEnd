9b8ae55379793f619c35271baae68c3d4e81c2cd
Gabriel Vianna <gabriel1_vianna@hotmail.com>
2020-09-24 19:09:50 -0300

    Fazendo requisição para o google para restaurantes proximos ao evento

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,21 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     origem: "",
     destino: "",
-    lat: "",
-    lng: "",
     evento: {
       nome: "",
       src: "",
       lugar: "",
-      descricao: ""
-    }
+      descricao: "",
+      lat: "",
+      lng: "",
+      restaurantes: []
+    },
   },
   getters: {
     getOrigem(state) {
@@ -25,7 +26,7 @@ export default new Vuex.Store({
     },
     getEvento(state) {
       return state.evento;
-    }
+    },
   },
   mutations: {
     SET_ORIGEM(state, payload) {
@@ -46,11 +47,14 @@ export default new Vuex.Store({
     SET_DESCRICAO(state, payload) {
       state.evento.descricao = payload;
     },
-    SET_LAT(state, payload){
+    SET_LAT(state, payload) {
       state.evento.lat = payload;
     },
-    SET_LNG(state, payload){
+    SET_LNG(state, payload) {
       state.evento.lng = payload;
+    },
+    adicionarRestaurantes(state, restaurantes){
+      state.evento.restaurantes.push(restaurantes);
     }
   },
   actions: {
@@ -68,8 +72,37 @@ export default new Vuex.Store({
     },
     setLng(context, payload) {
       context.commit("SET_LNG", payload);
-    }
+    },
+    async restaurantes({ state }) {
+      var myHeaders = new Headers();
+      myHeaders.append(
+        "X-API-Token",
+        "f549adb222bcd6e25c72fab51d17bca95706d7098ab6ad063e9905c97561612c.1592349843"
+      );
+      var raw = "";
+      myHeaders.append("Access-Control-Allow-Origin","*")
+      var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+
+      const proxyurl = "https://cors-anywhere.herokuapp.com/";
+      console.log(state.evento.lat)
+      console.log(state.evento.lng)
+
+      const response = await fetch(
+        proxyurl+`https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=1500&type=restaurant&location=${state.evento.lat},${state.evento.lng}&key=AIzaSyBQuJbfSamozKoKyFsnhYYgZIufPYNETME&=`,
+        requestOptions
+      )
+      const data = await response.json();
+      let restaurantes;
+
+      restaurantes.push(data.results[0], data.results[1], data.results[2], data.results[4])
+      
+      this.commit("adicionarRestaurantes", restaurantes);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
